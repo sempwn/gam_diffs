40ffbd8b9986428af9efd1af07942c0b9d02ae1d
sempwn <mike.irvine@bccdc.ca>
2022-07-20 11:18:56 -0700

    Added efron bootstrap. Fixed confidence interval vignette.

--- R/calc_from_bootstrap.R
+++ R/calc_from_bootstrap.R
@@ -87,3 +87,118 @@ calc_generic_vector_from_bootstrap <- function(m,
 
   return(list(m = diffs, lc = lc, uc = uc, total_n = total_n))
 }
+
+
+
+#' Calculate difference of vector of model inputs by bootstrapping original
+#' data
+#' @param m model object
+#' @param newdata data
+#' @param U difference matrix. defines the vector of differences being computed
+#' @param ci range of confidence interval
+#' @param nrep number of samples of posterior to compute ci
+#' @returns
+#' An `list` object. The output has the following elements:
+#'
+#' * m - mean
+#' * lc - lower confidence interval
+#' * uc - upper confidence interval
+#' * total_n - total number of samples in bootstrap
+#' @noRd
+#' @importFrom stats as.formula
+calc_generic_vector_from_efron_bootstrap <- function(m,
+                                               newdata,
+                                               U = NULL,
+                                               ci = 0.95,
+                                               use_relative_diff = FALSE,
+                                               nrep = 100) {
+
+  # extract original data to fit model
+  model_data <- m$model
+
+  npreds <- nrow(newdata)
+
+  input_check(npreds,U)
+
+  # get predictions
+  m_preds <- predict(m)
+
+  # get residuals
+  m_resids <- resid(m)#analytic_ed_data$counts - m_preds
+
+  ilink <- family(m)$linkinv
+
+  # reformulate formula to have y as response
+  formula_terms <- terms(m$formula)
+  term_labels <- attr(delete.response(formula_terms),"term.labels")
+  y_formula <- reformulate(term_labels,response = "y")
+
+  # flat vector version of 2 x npreds
+  V <- U[, 1] - U[, 2]
+  V <- matrix(V, ncol = npreds)
+
+  # baseline U vector used in relative change calculation
+  V_baseline <- U[, 2]
+  V_baseline <- matrix(V_baseline, ncol = npreds)
+
+  opt <- rep(NA, nrep)
+
+
+  # progress bar
+  pb <- progress::progress_bar$new(
+    format = "  bootstrapping [:bar] :percent eta: :eta",
+    total = nrep, clear = FALSE, width = 60
+  )
+
+  for (i in 1:nrep) {
+    sample_y <- m_preds + sample(m_resids, length(m_resids), replace = TRUE)
+    sample_data <- model_data
+    sample_data[, "y"] <- as.numeric(ilink(sample_y))
+
+    if(family == "poisson"){
+      sample_data[, "y"] <- rpois(sample_data[, "y"])
+    }
+
+    preds <- tryCatch(
+      {
+        m_sample <- mgcv::gam(as.formula(y_formula),
+                              data = sample_data,
+                              family = m$family
+        )
+
+        predict(m_sample, newdata = newdata, type = "response")
+      },
+      error = function(cond) {
+        return(rep(NA, npreds))
+      },
+      warning = function(cond) {
+        return(rep(NA, npreds))
+      }
+    )
+
+
+
+    opt[i] <- V %*% preds
+
+    if(use_relative_diff){
+      baseline_opt <- V_baseline %*% preds
+      opt[i] <- opt[i]/baseline_opt
+    }
+
+
+    pb$tick()
+  }
+
+  total_n <- sum(!is.na(opt))
+
+  diffs <- mean(opt, na.rm = TRUE)
+  lc <- stats::quantile(opt, probs = (1 - ci) / 2, na.rm = TRUE)
+  uc <- stats::quantile(opt, probs = 1 - (1 - ci) / 2, na.rm = TRUE)
+
+  return(list(m = diffs, lc = lc, uc = uc, total_n = total_n))
+}
+
+
+
+
+
